593e222ba788b61dda74f51911508ebf368204c6
Anderson Sanches <andycds@gmail.com>
2024-09-16 20:29:40 -0300

    Versão final do GeradorNumerosPrimos

--- src/main/java/br/pro/software/GeradorNumerosPrimos.java
+++ src/main/java/br/pro/software/GeradorNumerosPrimos.java
@@ -1,8 +1,6 @@
 package br.pro.software;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
